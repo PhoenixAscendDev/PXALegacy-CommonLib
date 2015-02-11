2a64daa528fbc494c2858393d136941a7392ef75
jbn566 <jbn566@gmail.com>
2015-02-10 23:39:36 -0500

    Add new Interfaces for Object

---
+++ C#/JB2Common/IObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JB2.Common
+{
+    public interface IObject<TKey, TTag> : IIDNamePair<TKey, string>
+    {
+        TTag[] Tags { get; set; }
+        bool AddTag(TTag tag);
+        bool RemoveTag(TTag tag);
+    }
+}

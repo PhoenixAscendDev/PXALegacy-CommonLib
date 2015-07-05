a4768fdb92013cd93fc05ce6ba94ef2b0cfa771a
jbn566 <jbn566@gmail.com>
2015-07-05 19:00:41 -0400

    Add IRepository

---
+++ C#/JB2Common/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JB2.Common.Interfaces
+{
+    public interface IRepository<T,Tkey>
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        T[] SearchFor();
+        T[] GetAll();
+        T GetById(Tkey id);
+        
+    }
+}

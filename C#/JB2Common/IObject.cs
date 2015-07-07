a8aeb1ddc4a210f7ac1c544312af1ef2882e4430
jbn566 <jbn566@gmail.com>
2015-07-07 03:37:33 -0400

    Add generic kind property to the object interface for all JB2 applications to help with unique signature

--- C#/JB2Common/IObject.cs
+++ C#/JB2Common/IObject.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace JB2.Common
 {
-    public interface IObject<TKey, TTag> : IIDNamePair<TKey, string>
+    public interface IObject<TKind,TKey, TTag> : IIDNamePair<TKey, string>
     {
+        TKind Kind { get; }
         TTag[] Tags { get; set; }
         bool AddTag(TTag tag);
         bool RemoveTag(TTag tag);

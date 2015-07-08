6d2f237e730a7d21a5c918cd03c35a709b4adf91
jbn566 <jbn566@gmail.com>
2015-07-07 22:18:57 -0400

    Add Helper static class for extensions

---
+++ C#/JB2Common/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Runtime.CompilerServices;
+
+
+namespace JB2.Common
+{
+    public static class Helper
+    {
+        public static T GetAttributeOfType<T>(this System.Enum enumVal) where T : System.Attribute
+        {
+            var type = enumVal.GetType();
+            var memInfo = type.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+    }
+}

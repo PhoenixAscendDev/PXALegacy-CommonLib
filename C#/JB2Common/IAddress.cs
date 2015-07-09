38589b3d6bf07309f353ceafb9cb26e7a269b3c4
jbn566 <jbn566@gmail.com>
2015-07-09 17:54:58 -0400

    New Interface for Businesses and and minor Address and Image interface

---
+++ C#/JB2Common/IAddress.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JB2.Common
+{
+    public interface IAddress
+    {
+        string FullAddress { get; set; }
+    }
+}
---
+++ C#/JB2Common/IBusiness.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JB2.Common
+{
+    public interface IBusiness<TKey> : IIDNamePair<TKey,string>
+    {
+        IPerson<TKey> POC { get; set; }
+        IAddress MailingAddress { get; set; }
+    }
+}

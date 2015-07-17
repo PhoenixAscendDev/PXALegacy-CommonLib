81d7f2e67e4d6bde7d32ff141e6ab21f12e59dd8
jbn566 <jbn566@gmail.com>
2015-07-17 03:29:37 -0400

    MS oAuth Test using cookies and no DB

---
+++ C#/JB2Auth/JB2.AuthorizationServer/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(JB2.AuthorizationServer.Startup))]
+
+namespace JB2.AuthorizationServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

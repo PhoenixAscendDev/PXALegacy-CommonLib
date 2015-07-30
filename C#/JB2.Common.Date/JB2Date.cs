9735764db607e26ba1e432610e162a7574193df3
jbn566 <jbn566@gmail.com>
2015-07-30 05:28:50 -0400

    Add JB2Date to Common Solution.  previously in API

---
+++ C#/JB2.Common.Date/JB2Date.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Globalization;
+
+using System.Reflection;
+
+namespace JB2.Common
+{
+    public class JB2Date
+    {
+        private int _datekey;
+        private string _dateStringFormat = "";
+        private DateTime _dt;
+        static readonly int MINKEY = 17500101;
+        static readonly int MAXKEY = 20201231;
+
+        public JB2Date(int dateKey)
+        {
+            if (!IsValidKey(dateKey))
+                _datekey = MINKEY;
+            else
+                _datekey = dateKey;
+            //base.serializableProperties = new List<string>();
+
+            //foreach(PropertyInfo p in this.GetType().GetProperties())
+            //{
+            //    base.serializableProperties.Add(p.Name);
+            //}
+           
+            //foreach(PropertyInfo p in this.GetType().)
+
+
+        }
+
+        #region Public Properties
+
+        public long Ticks
+        {
+            get { return ((DateTime)this).Ticks; }
+        }
+
+        public int DateKey
+        {
+            get
+            {
+                return _datekey;
+            }
+        }
+
+        public string DateUSA
+        {
+            get
+            {
+                return ((DateTime)this).ToString("MM/dd/yyyy");
+            }
+        }
+
+
+        public bool IsWeekend
+        {
+            get
+            {
+                var dt = (DateTime)this;
+                return (dt.DayOfWeek == DayOfWeek.Saturday || dt.DayOfWeek == DayOfWeek.Sunday);
+            }
+        }
+
+
+        public bool IsWeekday
+        {
+            get
+            {
+                return (!IsWeekend);
+            }
+            
+        }
+
+
+        public string DaySuffix
+        {
+            get
+            {
+                switch (DayOfMonth)
+                {
+                    case 1:
+                    case 21:
+                    case 31:
+                        return "st";
+                    case 2:
+                    case 22:
+                        return "nd";
+                    case 3:
+                    case 23:
+                        return "rd";
+                    default:
+                        return "th";
+                }
+            }
+        }
+
+        public string LongName
+        {
+            get
+            {
+                return ((DateTime)this).ToString("dddd, dd MMMM yyyy");
+            }
+        }
+
+
+
+
+        public byte DayOfMonth
+        {
+            get
+            {
+                return Convert.ToByte(_datekey.ToString().Substring(6, 2));
+            }
+        }
+
+        public byte Month
+        {
+            get
+            {
+                return Convert.ToByte(_datekey.ToString().Substring(4, 2));
+            }
+        }
+
+        public string MonthName
+        {
+            get
+            {
+                return ((DateTime)this).ToString("MMMM");
+
+            }
+        }
+
+        public short Year
+        {
+            get
+            {
+                return Convert.ToInt16(_datekey.ToString().Substring(0, 4));
+            }
+
+        }
+
+        public byte DayofWeek
+        {
+            get
+            {
+                var dt = (DateTime)this;
+                return (byte)dt.DayOfWeek;         
+            }
+        }
+
+        public string DayofWeekName
+        {
+            get
+            {
+                var dt = (DateTime)this;
+                return dt.DayOfWeek.ToString();
+            }
+        }
+
+        public byte DayofYear
+        {
+            get
+            {
+                var dt = (DateTime)this;
+                return (byte)dt.DayOfYear;
+            }
+        }
+
+        //[FullDateUK] CHAR(10), -- Date in dd-MM-yyyy format
+        //[FullDateUSA] CHAR(10),-- Date in MM-dd-yyyy format
+        //[DayOfMonth] VARCHAR(2), -- Field will hold day number of Month
+        //[DaySuffix] VARCHAR(4), -- Apply suffix as 1st, 2nd ,3rd etc
+        //[DayName] VARCHAR(9), -- Contains name of the day, Sunday, Monday 
+        //[DayOfWeekUSA] CHAR(1),-- First Day Sunday=1 and Saturday=7
+        //[DayOfWeekUK] CHAR(1),-- First Day Monday=1 and Sunday=7
+        //[DayOfWeekInMonth] VARCHAR(2), --1st Monday or 2nd Monday in Month
+        //[DayOfWeekInYear] VARCHAR(2),
+        //[DayOfQuarter] VARCHAR(3),
+        //[DayOfYear] VARCHAR(3),
+        //[WeekOfMonth] VARCHAR(1),-- Week Number of Month 
+        //[WeekOfQuarter] VARCHAR(2), --Week Number of the Quarter
+        //[WeekOfYear] VARCHAR(2),--Week Number of the Year
+        //[Month] VARCHAR(2), --Number of the Month 1 to 12
+        //[MonthName] VARCHAR(9),--January, February etc
+        //[MonthOfQuarter] VARCHAR(2),-- Month Number belongs to Quarter
+
+        #endregion Public Properties
+
+        #region implicit operators
+
+        public static implicit operator DateTime(JB2Date rhs)
+        {
+
+            //string strDate = rhs._datekey.ToString();
+            //System.Globalization.DateTimeFormatInfo dtfi = new System.Globalization.DateTimeFormatInfo();
+            //dtfi.ShortDatePattern = "yyyymmdd";
+            //dtfi.DateSeparator = "";
+            //DateTime objDate = Convert.ToDateTime(strDate, dtfi);
+            if(rhs._dt == DateTime.MinValue)
+                rhs._dt = new DateTime(rhs.Year, rhs.Month, rhs.DayOfMonth);
+
+            return rhs._dt;
+        }
+
+        public static implicit operator JB2Date(DateTime jb2d)
+        {
+            return new JB2Date(MINKEY);
+
+        }
+
+        public static implicit operator int(JB2Date rhs)
+        {
+            return rhs._datekey;
+        }
+
+        public static implicit operator JB2Date(int rhs)
+        {
+            return new JB2Date(rhs);
+        }
+
+        #endregion
+
+        #region Static Method
+
+        public static JB2Date  MinDate()
+        {
+            return new JB2Date(MINKEY);
+        }
+
+        public static JB2Date MaxDate()
+        {
+            return new JB2Date(MAXKEY);
+        }
+
+        public bool IsValidKey(int key)
+        {
+            bool result = true;
+            if (key < MINKEY || key > MAXKEY)
+                return false;
+            try
+            {
+                string strDate = key.ToString();
+                System.Globalization.DateTimeFormatInfo dtfi = new System.Globalization.DateTimeFormatInfo();
+                dtfi.ShortDatePattern = "yyyyMMdd";
+                dtfi.DateSeparator = "";
+                DateTime objDate = DateTime.ParseExact(
+                strDate,
+                "yyyyMMdd",
+                CultureInfo.InvariantCulture);
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+
+            return result;
+        }
+
+        #endregion Static Method
+
+
+        #region To Convert Methods
+
+        public DateTime ToDatetime()
+        {
+            return (DateTime)this;
+        }
+
+        public int ToInt()
+        {
+            return (int)this;
+        }
+
+        #endregion
+
+    }
+}

a52831b5e3d47d9d3c2a0701aae112872860d42c
jbn566 <jbn566@gmail.com>
2015-08-07 22:55:33 -0400

    Add new project for GoogleAnalytics

---
+++ C#/JB2.GoogleAnalytics/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2
+{
+    public static class GoogleAnalytics
+    {
+        static string _trackingID = string.Empty;
+        static readonly string _javascriptTracking = "@<script>(function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject'] = r; i[r]=i[r]||function(){(i[r].q = i[r].q ||[]).push(arguments)},i[r].l=1*new Date(); a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{0}', 'auto');ga('send', 'pageview');</script>";
+
+
+        static string TrackingID
+        {
+            get
+            {
+                return _trackingID;
+
+            }
+            set
+            {
+                _trackingID = value;
+            }
+        }
+
+        static string TrackingJavascript
+        {
+            get
+            {
+                return string.Format(_javascriptTracking, _trackingID);
+            }
+        }
+
+        static string GetTrackingJavascript(string trackingID)
+        {
+            return string.Format(_javascriptTracking, trackingID);
+        }
+
+
+
+
+
+    }
+}
---
+++ C#/JB2.Gravatar/TestConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using JB2.Common.Extensions;
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //JB2.Gravatar.GetImageUrlByEmail("bethiemoss@yahoo.com");
+
+            string emailAddress = "bethiemoss@yahoo.com";
+            var encoder = new System.Text.UTF8Encoding();
+            var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            var hashedBytes = md5.ComputeHash(encoder.GetBytes(emailAddress.ToLower()));
+            var emailHash = hashedBytes.ToHex(false);
+            //return GetImageUrl(emailHash, size, defaultImageurl);
+        }
+    }
+}

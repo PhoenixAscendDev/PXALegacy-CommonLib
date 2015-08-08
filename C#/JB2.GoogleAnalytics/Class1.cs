8bec54c685aa6d587a5761fbd77fccca933dad1b
jbn566 <jbn566@gmail.com>
2015-08-07 23:29:06 -0400

    fix issue with GoogleAnalytics static class

--- C#/JB2.GoogleAnalytics/Class1.cs
+++ C#/JB2.GoogleAnalytics/Class1.cs
@@ -9,10 +9,10 @@ namespace JB2
     public static class GoogleAnalytics
     {
         static string _trackingID = string.Empty;
-        static readonly string _javascriptTracking = "@<script>(function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject'] = r; i[r]=i[r]||function(){(i[r].q = i[r].q ||[]).push(arguments)},i[r].l=1*new Date(); a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{0}', 'auto');ga('send', 'pageview');</script>";
+        static readonly string _javascriptTracking = "<script>(function(i, s, o, g, r, a, m){{i['GoogleAnalyticsObject'] = r; i[r]=i[r]||function(){{(i[r].q = i[r].q ||[]).push(arguments)}},i[r].l=1*new Date(); a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)}})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{0}', 'auto');ga('send', 'pageview');</script>";
+       // static readonly string _javascriptTracking = "<script>{0}</script>";
 
-
-        static string TrackingID
+        public static string TrackingID
         {
             get
             {
@@ -25,7 +25,7 @@ namespace JB2
             }
         }
 
-        static string TrackingJavascript
+        public static string TrackingJavascript
         {
             get
             {
@@ -33,7 +33,7 @@ namespace JB2
             }
         }
 
-        static string GetTrackingJavascript(string trackingID)
+        public static string GetTrackingJavascript(string trackingID)
         {
             return string.Format(_javascriptTracking, trackingID);
         }

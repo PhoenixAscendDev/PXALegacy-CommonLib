8ccfc54d40819e78b318632ba501bee2d119e10f
jbn566 <jbn566@gmail.com>
2015-08-13 06:11:37 -0400

    ImageEntity for Azure Table Storage

---
+++ C#/JB2.Common.Data.AzureStorage/ImageEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Common.Data
+{
+    public class ImageEntity : Microsoft.WindowsAzure.Storage.Table.TableEntity
+    {
+        public ImageEntity(string partitionKey, string rowKey)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = rowKey;
+        }
+
+        public string Url { get; set; }
+        public string ThumbnailUrl { get; set; }
+
+    }
+}

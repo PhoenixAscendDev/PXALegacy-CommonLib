38a48b9aa0541691669794362ef106ef04f4fe18
jbn566 <jbn566@gmail.com>
2015-08-16 20:58:23 -0400

    relax the password rules.

--- C#/JB2Auth/JB2.AuthorizationServer/App_Start/IdentityConfig.cs
+++ C#/JB2Auth/JB2.AuthorizationServer/App_Start/IdentityConfig.cs
@@ -91,10 +91,10 @@ namespace JB2.AuthorizationServer
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults

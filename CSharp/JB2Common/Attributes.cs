e5f82c1f21636017c75631bec97210afe89b8b6f
Joshua Allen Bennett <jbn566@gmail.com>
2015-10-27 16:03:26 -0400

    Add ColorHex Attribute

---
+++ CSharp/JB2Common/Attributes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common.Attributes
+{
+    public class ColorHex : System.Attribute
+    {
+        public string Hex;
+
+        public ColorHex(string hex)
+        {
+            this.Hex = hex;
+        }
+    }
+}

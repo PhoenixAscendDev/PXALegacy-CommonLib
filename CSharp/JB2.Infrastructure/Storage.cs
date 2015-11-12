006b1af2e0dfd4250202ef21e125cf5b34abc516
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-11 21:27:16 -0500

    Setup an JBsquared Infrastructure project

---
+++ CSharp/JB2.Infrastructure/Storage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+using JB2.Common;
+using JB2.Common.Data;
+using Microsoft.WindowsAzure.Storage;
+
+namespace JB2.Infrastructure
+{
+    public static class Storage
+    {
+        private const string STORAGEKEYNAME = "JB2-StorageKey";
+        public static CloudStorageAccount GeneralCloudAccount
+        {
+            get
+            {
+                var storagekey = ConfigurationManager.AppSettings[STORAGEKEYNAME];
+
+                return AzureHelper.GetStorageAccount("jbsquared", storagekey);
+            }
+
+        }
+    }
+}

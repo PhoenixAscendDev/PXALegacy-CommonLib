b2e9a5b3d418719adca4ede2ad3da195bcbc4549
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-16 02:48:56 -0500

    Start using AzureKeyVault

--- CSharp/ConsoleTest/Program.cs
+++ CSharp/ConsoleTest/Program.cs
@@ -4,22 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure.KeyVault;
+using System.Threading;
+using System.IO;
+
 using JB2.Common;
 
 namespace ConsoleTest
 {
     class Program
     {
+
+
+        private async static Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(
+                ConfigurationManager.AppSettings["JB2:AD-clientID"],
+                ConfigurationManager.AppSettings["JB2:AD-clientSecret"]);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+
+            return result.AccessToken;
+        }
+
+        //KeyVaultKeyResolver cloudResolver = new KeyVaultKeyResolver(GetToken);
+
         static void Main(string[] args)
         {
-            //System.Drawing.Font f = JB2.Common.FontHelper.GetFont("ffft1");
+
+            Microsoft.Azure.KeyVault.RsaKey key = new RsaKey("private:key1");
+            System.Drawing.Font f = JB2.Common.FontHelper.GetFont("ffft1");
 
 
-            JB2.Common.RGB rgb = new RGB("fff1f1");
+            //JB2.Common.RGB rgb = new RGB("fff1f1");
 
-            Console.WriteLine(rgb.ToString());
+            //Console.WriteLine(rgb.ToString());
 
-            JB2.Common.JB2Color color = JB2Color.FromHex("6B4106");
+            //JB2.Common.JB2Color color = JB2Color.FromHex("6B4106");
             Console.ReadLine();
             //JB2.Common.Data.AzureHelper.AccountName = "jb2bowtie";
             //JB2.Common.Data.AzureHelper.AccountKey = "frIlemrNzlvAbKNhiyYCeW+otbFXBoJb0TodzbgwzF8IBEZMtifrHfx0Y+o1+jwvUL4FcAGYepHlgqrG0iCc1Q==";

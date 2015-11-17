2e29a4ad04ea4627c14b67ef4199fbd4ea91a117
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-17 06:05:31 -0500

    Add GeoLocation object to Common

---
+++ CSharp/JB2Common/GeoLocation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common
+{
+    public class Geolocation : IGeolocation
+    {
+        private double mLatitude { get; set; }
+        private double mLongitude { get; set; }
+
+        public Geolocation(double latitude, double longitude)
+        {
+            mLatitude = latitude;
+            mLongitude = longitude;
+        }
+
+        #region Public Members
+        public double Latitude
+        {
+            get
+            {
+                return mLatitude;
+            }
+        }
+        public double Longitude
+        {
+            get
+            {
+                return mLongitude;
+            }
+        }
+        #endregion Public Members
+
+        #region Public Methods
+
+        public double GetDistanceTo(IGeolocation othergeo)
+        {
+            throw new NotImplementedException();
+
+        }
+        #endregion Public Methods
+
+    }
+}

b48c5c7ecc15d274ee88facca4b735c99333fd69
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-17 05:08:36 -0500

    Improve the Address Interface - IAddress

--- CSharp/JB2Common/Interfaces/IAddress.cs
+++ CSharp/JB2Common/Interfaces/IAddress.cs
@@ -6,6 +6,16 @@ namespace JB2.Common
 {
     public interface IAddress
     {
+        string AddressLine1 { get; set; }
+        string AddressLine2 { get; set; }
+        string Building { get; set; }
+        string City { get; set; }
+        string CountryRegion { get; set; }
+        string FloorLevel { get; set; }
+        string PostalCode { get; set; }
+        string StateProvince { get; set; }
         string FullAddress { get; set; }
+        string CityAndState { get; }
+        IGeolocation GeoLocation { get; set; }
     }
 }

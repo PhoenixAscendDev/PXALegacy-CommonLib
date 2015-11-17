48cfba3a29e932757d96ad8a6727c1d1f93e1667
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-17 04:13:55 -0500

    Add IRange and SmallNumberRange

---
+++ CSharp/JB2Common/SmallNumberRange.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common
+{
+    public class SmallNumberRange : IRange<byte>
+    {
+
+        #region Fields
+
+        private byte _min;
+        private byte _max;
+
+        #endregion Fields
+        public byte Max
+        {
+            get
+            {
+                return _max;
+            }
+
+            set
+            {
+                _max = value;
+            }
+        }
+
+        public byte Min
+        {
+            get
+            {
+                return _min;
+            }
+
+            set
+            {
+                _min = value;
+            }
+        }
+
+        public bool IsWithinRange(byte value)
+        {
+            return (Enumerable.Range(_min, _max).Contains(value));
+        }
+    }
+}

353af0d55de77b8f1f510d5dd6d8122160c7bb83
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-17 06:37:12 -0500

    Seperated Address and GeoLocation to a new JB2.Common.Map projects

---
+++ CSharp/JB2.Common.Map/MapHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JB2.Common;
+
+namespace JB2.Common
+{
+    public static class MapHelper
+    {
+        public const double EarthRadiusInMiles = 3956.0;
+        public const double EarthRadiusInKilometers = 6367.0;
+
+        public static double ToRadian(double val) { return val * (Math.PI / 180); }
+        public static double DiffRadian(double val1, double val2) { return ToRadian(val2) - ToRadian(val1); }
+
+        public static double GetDistance(double lat1, double lng1, double lat2, double lng2)
+        {
+            return GetDistance(lat1, lng1, lat2, lng2, Enum.GeoDistanceType.Miles);
+        }
+
+        public static double GetDistance(double lat1, double lng1, double lat2, double lng2, Enum.GeoDistanceType m)
+        {
+            double radius = EarthRadiusInMiles;
+
+            if (m == Enum.GeoDistanceType.Kilometers) { radius = EarthRadiusInKilometers; }
+            return radius * 2 * Math.Asin(Math.Min(1, Math.Sqrt((Math.Pow(Math.Sin((DiffRadian(lat1, lat2)) / 2.0), 2.0) + Math.Cos(ToRadian(lat1)) * Math.Cos(ToRadian(lat2)) * Math.Pow(Math.Sin((DiffRadian(lng1, lng2)) / 2.0), 2.0)))));
+        }
+
+    }
+}

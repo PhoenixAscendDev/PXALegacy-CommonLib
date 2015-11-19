43620701e75942200738cce68c82f3c01f78e567
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-19 02:47:15 -0500

    Add new project.  JB2.Common.Scheduler

---
+++ CSharp/JB2.Common.Scheduler/DayOfTimeRange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Common
+{
+    public struct DayofWeekTimeRange
+    {
+        #region Fields
+
+        private System.DayOfWeek _dayofWeek;
+        private 
+       
+
+        #endregion Fields
+
+        #region Constructor
+        public DayofWeekTimeRange()
+        {
+
+        }
+
+        #region Constructor
+    }
+}

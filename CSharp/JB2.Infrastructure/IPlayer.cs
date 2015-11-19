2d1183ba3824b7b3d45c35154600781d3ce36591
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-19 03:49:47 -0500

    Add IPlayer to the Infrastructure Lib

---
+++ CSharp/JB2.Infrastructure/IPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using JB2.Common;
+namespace JB2.Infrastructure
+{
+    public interface IPlayer : JB2.Common.IPerson<string>
+    {
+        string MasterUsername { get; set; }
+        Email MasterEmail { get; set; }
+        string jBeanWalletID { get; set; }
+
+        int BitScore { get; set; }
+
+        string FamilyID { get; set; }
+    }
+}

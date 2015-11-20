f849625273499f1d3eb0e8684557d28193dfee17
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-20 07:30:52 -0500

    Add IJob interface

---
+++ CSharp/JB2.Common.Scheduler/IJob.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Common.Scheduler
+{
+    public interface IJob : IJob<string>
+    {
+
+    }
+    public interface IJob<TKey> : IJob<TKey,object>
+        where TKey : IComparable
+    {
+
+    }
+
+    public interface IJob<TKey,TParameter> : IDisposable, JB2.Common.IIDNamePair<TKey,string>
+        where TKey : IComparable
+    {
+        DateTime ExecutionTime { get; }
+        bool Repeat { get; }
+        bool InProgress { get; }
+
+        TParameter GetParameters();
+        void Start();
+        void Cancel();
+        ServiceResult DoWork();
+
+
+
+        event Action<IJob<TKey, TParameter>> Started;
+        event Action<IJob<TKey, TParameter>> Completed;
+        event Action<IJob<TKey, TParameter>, int> ProgressChanged;
+    }
+}

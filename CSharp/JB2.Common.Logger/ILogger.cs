7ea50592cd05745786481dff8c1d6a9d5cb2654b
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-20 07:49:58 -0500

    Add new generic Logger Project

---
+++ CSharp/JB2.Common.Logger/ILogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JB2.Common.Log.Enum;
+
+namespace JB2.Common.Log
+{
+
+    public interface ILogger : ILogger<LogServerityType>
+    {
+
+    }
+
+
+    public interface ILogger<TServerity>
+        where TServerity : IComparable
+    {
+        bool IsDebugEnabled { get; }
+        bool IsVerboseEnabled { get; }
+        bool IsInformationalEnabled { get; }
+        bool IsWarningEnabled { get; }
+        bool IsFatalEnabled { get; }
+        bool IsErrorEnabled { get; }
+
+        void Write(string message, TServerity severity);
+        void Write(string message, Exception exception, TServerity severity);
+    }
+}

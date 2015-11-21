feb68d641d3bfb3f702f61ee752e43939c1d6019
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-20 22:39:57 -0500

    Logger abstract helper class as well as start Elmah logger project

---
+++ CSharp/JB2.Common.Logger/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JB2.Common.Log.Enum;
+
+namespace JB2.Common.Log
+{
+    public abstract class Logger : ILogger
+    {
+        private Dictionary<Enum.LogServerityType, bool> _serverityFlag = new Dictionary<LogServerityType, bool>();
+
+        public virtual bool IsEnabled(LogServerityType serverity)
+        {
+            if (!_serverityFlag.ContainsKey(serverity))
+                return false;
+            else
+                return _serverityFlag[serverity];
+        }
+
+        public void SetServerityType(Enum.LogServerityType type, bool enable)
+        {
+            if (!_serverityFlag.ContainsKey(type))
+                _serverityFlag.Add(type, enable);
+            else
+                _serverityFlag[type] = enable;
+        }
+
+        public abstract void Log(ILogEntry entry);
+    }
+}

5b0e8b4bab77446179cf915c23878873295dd5b7
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-22 11:31:34 -0500

    Seperate the JB2 Player to own project for faster updates and reduce  dependencies

---
+++ CSharp/JB2.Common.Player/IPlayerRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common
+{
+    public interface IPlayerRepo : IRepository<IPlayer,string>
+    {
+
+    }
+}

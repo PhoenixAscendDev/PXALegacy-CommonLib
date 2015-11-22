8f9efddeb09b22d4c623dfdfbc3e1aff700ec4f1
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-22 13:18:58 -0500

    Add playerprofile to repo

--- CSharp/JB2.Common.Player/IPlayerRepo.cs
+++ CSharp/JB2.Common.Player/IPlayerRepo.cs
@@ -8,6 +8,10 @@ namespace JB2.Common
     public interface IPlayerRepo : IRepository<IPlayer,string>
     {
 
+        void Insert(PlayerProfile profile);
+        void Delete(PlayerProfile profile);
+        PlayerProfile GetPlayerProfileByID(string id);
+        IEnumerable<PlayerProfile> GetPlayerProfileByPlayerID(string playerid);
 
 
     }

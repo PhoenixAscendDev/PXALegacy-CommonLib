e893c3ab3e6ab637f5eacebc915bd95ff23c67e2
jbn566 <jbn566@gmail.com>
2015-11-24 08:58:45 -0500

    IScheduler interface for common scheduler

---
+++ CSharp/JB2.Common.Scheduler/IScheduler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Common.Scheduler
+{
+
+    public interface IScheduler: IScheduler<string,object>
+    { }
+    public interface IScheduler<TJobKey,TJobParameter>
+        where TJobKey: IComparable
+    {
+        IEnumerable<IJob<TJobKey, TJobParameter>> GetJobs();
+        void Add(IJob<TJobKey, TJobParameter> job);
+        void Remove(IJob<TJobKey, TJobParameter> job);
+
+        JB2.Common.Log.ILogger GetLogger();
+
+        void DoJobs();
+        void StopJobs();
+
+        event Action<IScheduler<TJobKey,TJobParameter>> Started;
+    }
+}

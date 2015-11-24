f3986a8ef107caa8b0752fcc26cfbfae695c05bb
jbn566 <jbn566@gmail.com>
2015-11-24 05:51:37 -0500

    Add NotConfigured Exception to Common.Core

---
+++ CSharp/JB2.Common.Core/Exceptions/NotConfiguredException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Common.Exceptions
+{
+    public class NotConfiguredException : Exception
+    {
+        public NotConfiguredException()
+            : this("Setting was not configured")
+        {
+
+        }
+
+        public NotConfiguredException(string message) : this(message, null)
+        {
+
+        }
+
+        public NotConfiguredException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

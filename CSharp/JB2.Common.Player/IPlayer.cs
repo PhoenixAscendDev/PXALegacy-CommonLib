688cb06c569f9b5e47c85ac9d8982b3141976cf6
Joshua Allen Bennett <jbn566@gmail.com>
2015-11-25 11:55:47 -0500

    Update nuget packages

--- CSharp/JB2.Common.Player/IPlayer.cs
+++ CSharp/JB2.Common.Player/IPlayer.cs
@@ -7,10 +7,16 @@ namespace JB2.Common
 {
     public interface IPlayer : JB2.Common.IPerson<string>, IMetaDatable
     {
-        string MasterUsername { get; set; }
-        Email MasterEmail { get; set; }
-        string jBeanWalletID { get; set; }
-        int BitScore { get; set; }
-        string FamilyID { get; set; }
+        IMetaData GetModuleMetaData(string module, string propertyName);
+
+        string GetMasterUsername();
+        string GetMasterEmail();
+
+        string GetjBeanAccountNumber();
+
+        int GetBitScore();
+
+        string GetFamilyID();
+
     }
 }

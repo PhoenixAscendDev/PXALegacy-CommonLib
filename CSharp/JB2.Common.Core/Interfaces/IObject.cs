fba34d036697e9973a0ff8af11bfdcccb596f3e6
Joshua Allen Bennett <jbn566@gmail.com>
2015-12-17 16:23:30 -0500

    Tagable  and AzureTableEntry clean up some Interface

--- CSharp/JB2.Common.Core/Interfaces/IObject.cs
+++ CSharp/JB2.Common.Core/Interfaces/IObject.cs
@@ -4,12 +4,19 @@ using System.Text;
 
 namespace JB2.Common
 {
-    public interface IObject<TKind,TKey, TTag> : IIDNamePair<TKey, string>
+    public interface IObject<TKind,TKey,TTag> : IObject<TKind,TKey,TTag,DateTime>
+        where TKey : IComparable
+    {
+
+    }
+
+    public interface IObject<TKind,TKey, TTag,TUpdate> : 
+            IIDNamePair<TKey, string>,
+            IUpdateable<TUpdate>,
+            ITagable<TTag>
          where TKey : IComparable
     {
-        TKind Kind { get; }
-        TTag[] Tags { get; set; }
-        bool AddTag(TTag tag);
-        bool RemoveTag(TTag tag);
+        TKind GetKind();
+        //TKind Kind { get; }
     }
 }
---
+++ CSharp/JB2.Common.Core/Interfaces/IUpdateable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common
+{
+    public interface IUpdateable : IUpdateable<DateTime>
+    {
+
+    }
+    public interface IUpdateable<T>
+    {
+        T GetLastUpdate();
+    }
+}

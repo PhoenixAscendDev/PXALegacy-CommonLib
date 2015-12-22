f23edfb128860a4810e7c65f24ea885e6c7cc05f
jbn566 <jbn566@gmail.com>
2015-12-21 22:43:11 -0500

    Modify the Version Number

--- CSharp/JB2.Common.Core/Extensions/DataExtenstions.cs
+++ CSharp/JB2.Common.Core/Extensions/DataExtenstions.cs
@@ -59,9 +59,24 @@ namespace JB2.Common.Extensions
 
             return new System.Collections.BitArray(marks.ToArray());
         }
+    }
 
-
-
+    public static class ObjectExtensions
+    {
+        public static bool IsNumber(this object value)
+        {
+            return value is sbyte
+                    || value is byte
+                    || value is short
+                    || value is ushort
+                    || value is int
+                    || value is uint
+                    || value is long
+                    || value is ulong
+                    || value is float
+                    || value is double
+                    || value is decimal;
+        }
     }
 
     public static class JB2Extensions
--- CSharp/JB2.Common.Core/VersionNumber.cs
+++ CSharp/JB2.Common.Core/VersionNumber.cs
@@ -3,35 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
+using JB2.Common.Extensions;
 namespace JB2.Common
 {
-    //public struct VersionNumber: VersionNumber<byte,byte,byte,int>
-    //{
-
-    //}
-    public struct VersionNumber<TMajor,TMinor,TBuild,TRevision>
+    public class VersionNumber : VersionNumber<int, int, int, int>
     {
-        #region Fields
-        private TMajor _major;
-        private TMinor _minor;
-        private TBuild _build;
-        private TRevision _revision;
-        private bool _isPrerelease;
-        private string _format;
-        #endregion Fields
-
-        #region Constructors
-        public VersionNumber(TMajor major, TMinor minor, TBuild build, TRevision revision)
+        public VersionNumber(int major, int minor, int build, int revision) : this()
         {
             _major = major;
             _minor = minor;
             _build = build;
             _revision = revision;
-            _isPrerelease = false;
-            _format = "{0}.{1}.{2}.{3}";
         }
+
+        public VersionNumber(int major, int minor) : this(major, minor, 0, 0)
+        {
+
+        }
+
+        public VersionNumber(System.Version version) : this()
+        {
+            _major = version.Major;
+            _minor = version.Minor;
+            _build = version.Build;
+            _revision = version.Revision;
+        }
+
+        public VersionNumber()
+        {
+            _format = "{0}.{1}.{2}.{3}";
+            _isPrerelease = false;
+        }
+
+        public static implicit operator int(VersionNumber v)
+        {
+            int result = (v.Major * 100000000) + (v.Minor * 1000000) + (v.Revision * 10000) + (v.Build);
+            return result;
+        }
+
+        public override int ToInt()
+        {
+            return (int)this;
+        }
+
+    }
+    public abstract class VersionNumber<TMajor,TMinor,TBuild,TRevision> : IComparable
+        where TMajor: IComparable
+        where TMinor: IComparable
+        where TBuild: IComparable
+        where TRevision: IComparable
+    {
+        #region Fields
+        protected TMajor _major;
+        protected TMinor _minor;
+        protected TBuild _build;
+        protected TRevision _revision;
+        protected bool _isPrerelease;
+        protected string _format;
+        #endregion Fields
+
+        #region Constructors
+
         #endregion Constructors
 
+        #region Properties
         public TMajor Major
         {
             get { return _major; }
@@ -62,17 +98,32 @@ namespace JB2.Common
             set { _isPrerelease = value; }
         }
 
+        #endregion Properties
+
+        #region ToString
         public override string ToString()
         {
-            return string.Format(_format, _major.ToString(), _minor.ToString(), _build.ToString(), _revision.ToString());            
+            return ToFormattedString(_format);
         }
 
+        #endregion ToString
 
+        public abstract int ToInt();
 
+        public virtual string ToFormattedString(string format)
+        {
+            string major = _major.IsNumber() ? Convert.ToInt16(_major).ToString("D2") : _major.ToString();
+            string minor = _minor.IsNumber() ? Convert.ToInt16(_minor).ToString("D2") : _minor.ToString();
+            string build = _build.IsNumber() ? Convert.ToInt16(_build).ToString("D2") : _build.ToString();
+            string revision = _revision.IsNumber() ? Convert.ToInt16(_revision).ToString("D2") : _revision.ToString();
 
+            return string.Format(format, major, minor, build, revision);
+        }
 
-
-        #endregion Constructors
-
+        public virtual int CompareTo(object obj)
+        {
+            VersionNumber v = (VersionNumber)obj;
+            return v.ToInt().CompareTo(this.ToInt());
+        }
     }
 }

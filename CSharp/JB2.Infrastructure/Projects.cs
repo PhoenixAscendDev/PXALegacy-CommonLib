753fc9ca9b77d87197d05da68a56ffc0ee700700
Joshua Allen Bennett <jbn566@gmail.com>
2015-12-22 03:35:44 -0500

    setup hook for project code for app

--- CSharp/JB2.Infrastructure/Projects.cs
+++ CSharp/JB2.Infrastructure/Projects.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Configuration;
+
 namespace JB2.Infrastructure
 {
     public class Projects
     {
+        private const string PRODJECTCODE = "JB2:projectCode";
+
         public static JB2Project  Project(string projectCode)
         {
             return new JB2Project(projectCode);
+        }
+        public static JB2Project CurrentProject
+        {
+            get
+            {
+                return new JB2Project(ConfigurationManager.AppSettings[PRODJECTCODE]);
+            }
         }
     }
 }

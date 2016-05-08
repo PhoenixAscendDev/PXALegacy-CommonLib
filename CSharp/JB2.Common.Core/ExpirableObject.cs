58abe44f2f3973eabc7d7c7ee55e911a63a25813
Joshua Allen Bennett <jbn566@gmail.com>
2016-05-08 17:17:25 -0400

    Expirable

---
+++ CSharp/JB2.Common.Core/ExpirableObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common
+{
+    public abstract class ExpireableObject: ExpirableObject<object>
+    {
+
+    }
+    public abstract class ExpirableObject<TValue> : IExpirable<TValue>
+    {
+
+        #region Constructors
+
+        #endregion Constructors
+        public virtual DateTime Expires
+        {
+            get
+            {
+                return TimeStamp + TimeToLive;
+            }
+            set
+            {
+                TimeToLive = value - TimeStamp;
+            }
+        }
+
+        public virtual bool HasExpired
+        {
+            get
+            {
+                return DateTime.Now > Expires;
+            }
+        }
+
+        public virtual DateTime TimeStamp
+        {
+            get; set;
+        }
+
+        public TimeSpan TimeToLive
+        {
+            get; set;
+        }
+
+        public TValue Value
+        {
+            get;set;
+        }
+    }
+}
---
+++ CSharp/JB2.Common.Core/Interfaces/IExpirable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common
+{
+    //http://www.jondavis.net/techblog/post/2010/08/30/Four-Methods-Of-Simple-Caching-In-NET.aspx
+
+    public interface IExpirable<TValue>
+    {
+        TValue Value { get; set; }
+        DateTime TimeStamp { get; set; }
+        TimeSpan TimeToLive { get; set; }
+        DateTime Expires { get; set; }
+        bool HasExpired { get; }
+    }
+}

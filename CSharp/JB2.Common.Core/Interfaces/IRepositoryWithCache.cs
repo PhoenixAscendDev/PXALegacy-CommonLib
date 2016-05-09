48a18fd0cfe88c6fbcc71acaa78e486d0318108e
Joshua Allen Bennett <jbn566@gmail.com>
2016-05-08 21:33:38 -0400

    RepositoryWithCache internface

---
+++ CSharp/JB2.Common.Core/Interfaces/IRepositoryWithCache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common.Interfaces
+{
+    public interface IRepositoryWithCache<TEntity, TKey, TSearchFilter> : IRepository<TEntity, TKey, TSearchFilter>
+    {
+        int DefaultMinutesToLive { get; set; }
+
+        bool ClearCache();
+        TEntity[] SearchFor(bool useCache = true);
+
+        TEntity[] SearchFor(TSearchFilter filter, bool useCache = true);
+        TEntity[] GetAll(bool useCache = true);
+        TEntity GetById(TKey id, bool useCache = true);
+
+
+    }
+}

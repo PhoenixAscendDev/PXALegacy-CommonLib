5c84a7462501def24e77dcb42a5b6051a8cd703f
Joshua Allen Bennett <jbn566@gmail.com>
2016-08-01 00:30:39 -0400

    updating nuget packages

--- CSharp/Infrastructure/JB2.Infrastructure.Test/Program.cs
+++ CSharp/Infrastructure/JB2.Infrastructure.Test/Program.cs
@@ -11,18 +11,22 @@ namespace JB2.Infrastructure.Test
         static void Main(string[] args)
         {
             // var list = JB2.Infrastructure.Counter.GetNext("dewdrop", 10, 1000000);
-            var id = JB2.Infrastructure.Counter.GetNext("fivetwo_entryitem");
-            Console.WriteLine(id);
+            //var id = JB2.Infrastructure.Counter.GetNext("fivetwo_entryitem");
+            //Console.WriteLine(id);
 
-            for (int i = 1; i <= 20; i++)
-            {
+            //for (int i = 1; i <= 20; i++)
+            //{
                 
-            }
+            //}
+
+            var result = JB2.Configuration.GetAppSetting("JB2:gerbigStorageKey");
+
+            Console.WriteLine(result);
             //foreach(long i in list)
             //{
             //    Console.WriteLine( JB2.Common.NewID.Base62(i));
             //}
-                       
+
             Console.ReadLine();
         }
     }

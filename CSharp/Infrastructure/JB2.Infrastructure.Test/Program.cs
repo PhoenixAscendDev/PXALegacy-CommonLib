d4b7562b20402b30deef8e0b48d26bea911dd4ff
Joshua Allen Bennett <jbn566@gmail.com>
2017-01-15 16:25:01 -0500

    Work on an alternate Encrypt procedure

--- CSharp/Infrastructure/JB2.Infrastructure.Test/Program.cs
+++ CSharp/Infrastructure/JB2.Infrastructure.Test/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 using JB2.Common.Log;
 
@@ -10,10 +11,11 @@ namespace JB2.Infrastructure.Test
 {
     class Program
     {
+
         private static Random random = new Random();
         public static string RandomString(int length)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
@@ -21,24 +23,62 @@ namespace JB2.Infrastructure.Test
         {
 
             string plainText = "nEyMxJJswzxIDHzswCvyBDyIMFtCHEwDHEHGKrDqoJwxotGMuyMnquuvyApAzIxnMDzrtJqHxsxzqtLLDIFLzvHDnzMIHxrvAwGrCnAwoHHzzusMsvMHsnrpDsExsAstAMxzIJLBqJsJtwMpMzJCpAsKztzwvKvMupuwEDnyCKMoqGnqyoEEwBxKBAozHxxxLsrwDyqsqvHsxxqEpLyBzoFyABLwurHEtwnGosDBEzLsMsCGGowxCLLMDCtqJsCvzntxoqGGuFBGKnDtJrFnEFHwuHDqsqvDCJqIsCoxqHJAtsMAsAsBxqEFAwxrFxrJyIwrLrzMJrKsIvJLtGDxMHKtvHLoGssAoLGIrpvBEwvKvsFDBxAoJuxyqHAFBpGIDAMzIJFEIICqFywrDCnrqtyGGBoHJzJzxzpquEpJwoxzyuKssqBBwsvtBnGyGwELJpICuEApLBGFKBMoxAoJoonrrCuKqsnBMAtKBHvnuCIuoKpJEuxE";
+             plainText = "a";
+            // ...
 
-            for (int i = 0; i < 2000; i++)
+            Stopwatch sw = new Stopwatch();
+
+            int testloop = 10;
+            sw.Start();
+            for (int i = 0; i < testloop; i++)
             {
                 plainText = RandomString(400);
+                plainText = "aaaaa";
                 string encryptText = JB2.Infrastructure.Cipher.Encrypt(plainText);
+
+                //Console.WriteLine("plain:" + "(" + plainText.Count() + ")" + plainText);
+                //Console.WriteLine("");
+                //Console.WriteLine("encrypt:" + "(" + encryptText.Count() + ")" + encryptText);
+                //Console.WriteLine("");
                 StringBuilder sb = new StringBuilder();
-                foreach (char c in encryptText)
-                    sb.AppendFormat("0x{0:X2} ", (int)c);
-                var encryptHEx = sb.ToString().Trim();
+                //foreach (char c in encryptText)
+                //    sb.AppendFormat("0x{0:X2} ", (int)c);
+                //var encryptHEx = sb.ToString().Trim();
                 //Console.WriteLine(JB2.Common.NewID.ProductID());
                 //Console.WriteLine(plainText);
                 //Console.WriteLine(encryptText);
                 string decryptText = JB2.Infrastructure.Cipher.Decrypt(encryptText);
                 //Console.WriteLine(plainText);
-                //Console.WriteLine(encryptText);
-                //Console.WriteLine(decryptText);
-                Console.WriteLine(plainText == decryptText);
+                //Console.WriteLine();
+                //Console.WriteLine("decript:" + "(" + decryptText.Count() + ")" + decryptText);
+                //Console.WriteLine("");
+               Console.WriteLine( (plainText == decryptText) && (plainText != encryptText));
             }
+
+            sw.Stop();
+
+            Console.WriteLine("Elapsed={0}", sw.Elapsed);
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < testloop; i++)
+            {
+                plainText = RandomString(400);
+
+                var et = JB2.Infrastructure.Cipher.Encrypt2(plainText);
+
+               //Console.WriteLine("----Decrypt----");
+
+                var dt = JB2.Infrastructure.Cipher.Decrypt2(et);
+
+                Console.WriteLine((plainText == dt) && (et != plainText));
+            }
+
+            sw.Stop();
+            Console.WriteLine("Elapsed={0}", sw.Elapsed);
+
+
+
             Console.ReadLine();
         }
 
--- CSharp/JB2.Infrastructure/Cipher.cs
+++ CSharp/JB2.Infrastructure/Cipher.cs
@@ -8,7 +8,116 @@ namespace JB2.Infrastructure
 {
     public static class Cipher
     {
-        
+        private const string mk1 = "JGsrCuIwGnMDxnntqCInGyAzrvEtrzpLIKGpowwvAEwCryLxCAxwMBFzwJyupyGoxDKpwsztnFBAspCsyEnCtBKBxuwtzqFLoInsoFHvtHynzsMsBMnxoypzyKqzLrotLIsEMJqzBEvIAExHzFLIFoHHIszpyDJAEtMyurLwEJGAorsMwJpFtvtxMJxDvFFtFFyBqzAxnGFLHKrAKxBxMKFEBwHtpMqHorIIvzrqLJpHnwIErwMtwnqxLsIJsLoKrvGwvMMCAoLHJussIBrLxJxuMMDEttFpCCqrzzFFCLutvLIxzEKqxvsuyvvLuHuHwBzGBsKBDvvvLqurFBMnwHrrAtGFoDEBIIGMBuvnvMwBKCoMGtvFvFvILFpvxzvozALGJvDBztMpsEtuGxwtvKMIBwAFKCwDuItyMpoJGLBAwCttEsvIswFBBFAnLzGpsoFnCJzCCMuByssywLuqtqzpsJEADCyoJqtIHCszArEHFvqqvzxtsHHvLAIuMArpvCKnqBpCyoLrvnHCMxxEpEDxAzzuwEHFtsnrMrvLsHBnozwIKnrpFGBqMDsxsEswDIornArzEJzpnCGqxLCuqGJHpGnyuKAwBJJFHKvxMsGLLBnzDKpCDuAxzKqIGJCrJJDuEEnxAtDwByrCrDwKEzMvtIDGBsMsHsArHEnLoDtDHpLuFMMytHwHKywBnCEqIoyFJAxrnBwEAxKKJJMAKGAzMsozJtsAFJIAMGvwvpMMuGGyBDBnJLzpzqpMHLzrxwyyryKHGquGtosAyzCFHqErnHzBqFAwyuwquJDxLLErAFnEDJEsDBGsAxHLLoEyJrHKLMDJrsvLwDBEwzMAFosLnFqvqwqywKqEJCFqzGLvtLuGtLDAMBBFuJqvtJsvqMLBzMpIAHGACGsnpDHxIAvwAoFtCqIzHnMznKuGJDFrDzDzrwCxtoBAsJtMKKDAqxtnwLvnyEItroztyMqxqHFDBCKBIqKAvwzMMDFBuyGwwHvBwopBxMwMEIuGBFuoFJtrHyxJoJKprEqGwvJrDqLovJwEsoMBMIsvFDEIxqoMIupKnuJstrMKpFnKtHsuIysrwoDKxsIBnEoGpEBMLtAIwAGpALtntCnMrvzyqtxBJsHtqunozAnFqEMyAEEEutDBJyvprGGKzIFnpqJJKFEznDCGMtHwzwvtLMyoqAGnzJCpIrLKCttFHGtMpvEEnrtDuKGGEwAsrsLDIpnMzIyHtJAyrsyyMLpKvAIqqEDAJqDLvvpJsvJrqpuAoJoLtGArxzzJnpxyJoIKqAqoLuKpzqytvrHBpnGCLsoyJKxtrrCuArKBqLDLMrAssGLAGvrqvzGLGEMFCvCrxLEBnMyJwsqyCKnwHAztFMsFrIEJHFIKGCwHCFvxMxMGCqvBJxAKnHFMLupMKnKuMwpHwtJsBxzHJyuIsCwMrxuGzHpwwoqByAGuJvGLBFuMvLEnBKtxqxLHGDxrtuyryGrMHsnyLDGAoKIzwJMwExGJHAnBvJBxBKGJEKnBvECDBqzusoBHxKsJAAJEIoFIxypxHnzDFAszHxLvKpvuuBLLHDxquxArxzHCEEAIuFpMpCtxyqEKGxvpoGqFppCLGCGqMGpDoKvrvFIvoHwIyFxxvFBJoJILuvvAoDuJCqEJxyJywLEtpwvxquErwHpoHEutAJDtqwntwLtGJryLLwDJDJIrIDtwnCMssCDFuJuAEyInnouHynMBHCoBLypxBHKItyEpzJxnKsrIHznBFByHwwEEsFMxrEEHLDFLKsFvqnLJqsLvysoDuznyHrwowLsMtvKILrGnIHBBGLFxtorDDLxvwvtoupIxAMnuwvMstutDIEFpFuJMAyBvBCxHpqxCsMDAAuIFrIvFsHtzvtsBLqxApzwGIuMwtMEnzCKKBopqFCJuKLJMurvzCpzJMzrLnpDCDxHDvKuKCyJMnBKrFrFGIJMuKqHFJunovMntunpqMzrJyJCEBEBDBwJKEMqyMnDCCqornustIHLGunxvrqpFzpopyHDHxEFyuDrwCyFzHpyyLLttGFFDuGHJuMBBHJHstMAtuBFyCwFvyDxsIxDBzBDxunAGpyJLpEIvEsopnJMJyHzKzuJMyBHpxBMwsMFHIEKHszzpotnqxnoIMxJGKDHCqMFvMJBGIuuKBHsHFGLLxzIEotqyLJHxwLrJprEutAsIwvCByxqJDLAGLBHMpKuGquMuHDyqJoGvwpoxrGwBvAEvotDHpJwxMKytEyMuGEItGztvMxLzDGuuyIvqHtILsDAuMwqptsJrDLzzCoFCprnxnCMFrtsHqLMDxAvrJFoFyDqCqKHxHLHIwzKrxJLKCIsqxwvwAqMuvMEqrMuCzButDLnFHrMvyMCLvDyzFGICAKrBpGILpGqHECGMvvwyLnInxDGKLvJxJvuArArvHsCCoHHKsqoqyDHGMBupzFCuBoJEBAwpoyuDLEvLEDnKnpnIzDAHyFnFHMrDsowBBtEuHLDnuuFFGyMICALnLpMtqsJFGGCwtDqsvppzEzKLAHonKznFqFtxxKyEBwGCKMuFopzyMrCrsAsFAMDMMtzzwwItHrGzJuyuyMzqLwvuopLKqqBzxzyqzqvKuJqrpoprJDpICCEuKCMqDIsMxFJzGwzBFxtCHpsBAwMEKzuxGyAwDHHAMrFvuuKCzqzsyqGuxxCrpMCCGwEnotptotnwBpAAAGrowDxnAKnsowAIJMnozvryxFBrFpKoIIsxHwIwFLrGytywLsKMMxAzrtJrBKwxItEwtMLJsoKLsMxvsuIHoBpBAMnGtFuIJJMtAqxGpnFovAGwoGJMKHDAoqvsqJJMonAqBMwyBvEEInJnJsnzMqGqKpKuDvECAGxJxtquFJzqyArqyyxztHtEptrvGwwLpEJunBMEvzMBnvxCuEwFAJJoqDJusCGvtEuAGrvBxMAAAFAEszMBtoHFFqDMnEAoMDqpKwDxFFnrDKtLuvIooMCzLyKInvxEzEnnMEsyFEwortzGqEEFuL";
+
+        public static string Encrypt2(string plainText)
+        {
+            var alphabet = JB2.Common.Cipher.AllAlphabet;
+            var pk = JB2.Common.RNG.ThreadSafe(alphabet.Length);
+            var i = pk % 3;
+            var keyIndexSettingName = "JB2:Cipher:" + i.ToString();
+            //Console.WriteLine(plainText);
+            var mk = JB2.Configuration.GetAppSetting(keyIndexSettingName);
+
+           // var mk = mk1;
+//
+            int[] ki = new int[6] { 0, 505, 1010, 1515, 2020, 2525 };
+
+            var r = JB2.Common.RNG.ThreadSafe(ki.Count());
+            int p = ki[r];
+
+            //Console.WriteLine("p:" + p);
+            string k = mk.Substring(p, 4);
+           // Console.WriteLine("k:" + k);
+
+            int k_ascii = 0;
+
+            foreach (var c in k.ToCharArray())
+            {
+                k_ascii = k_ascii + (int)c;
+            }
+
+            int m = (k_ascii % 4);
+
+            int o = ki[m];
+
+            var key = mk.Substring(o + 4,500);
+
+           // Console.WriteLine("key:" + key);
+            //Console.WriteLine("");
+
+            var encryptKey = cindyEncrypt(plainText, key);
+
+            //Console.WriteLine("encrptkey:" + encryptKey);
+            //Console.WriteLine("");
+
+            var final = alphabet[pk] + encryptKey + k;
+
+            Console.WriteLine("final:" + final);
+           Console.WriteLine("");
+
+            return final;
+
+        }
+
+        public static string Decrypt2(string encyptText)
+        {
+            char[] alphabet = JB2.Common.Cipher.AllAlphabet;
+            int[] ki = new int[6] { 0, 505, 1010, 1515, 2020, 2525 };
+
+            var s = encyptText[0];
+
+            int pk = Array.IndexOf(alphabet, s);
+
+            int i = pk % 3;
+
+
+
+            var keyIndexSettingName = "JB2:Cipher:" + i;
+            var mk = JB2.Configuration.GetAppSetting(keyIndexSettingName);
+            //var mk = mk1;
+            var k = encyptText.Substring(Math.Max(0, encyptText.Length - 4));
+
+            int k_ascii = 0;
+
+            foreach (var c in k.ToCharArray())
+            {
+                k_ascii = k_ascii + (int)c;
+            }
+
+            int m = (k_ascii % 4);
+
+            int o = ki[m];
+
+            var key = mk.Substring(o + 4,500);
+
+            //Console.WriteLine("");
+           // Console.WriteLine("k:" + k);
+
+            encyptText = encyptText.Substring(1);
+            encyptText = encyptText.Substring(0, encyptText.Length - 4);
+            //Console.WriteLine("");
+            //Console.WriteLine("encyptText:" + encyptText);
+
+            int p = -1;
+
+            //foreach (int o in ki)
+            //{
+            //    if (mk.Substring(o, 4) == k)
+            //        p = o;
+            //}
+
+            //var key = mk.Substring(p);
+
+            //Console.WriteLine("");
+            //Console.WriteLine("key:" + key);
+
+            var plainText = cindyDecrypt(encyptText, key);
+
+            return plainText;
+        }
+
+
         public static string Encrypt(string plainText)
         {
             //get key index

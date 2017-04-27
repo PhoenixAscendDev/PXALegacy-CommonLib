50b2b8f32a353bb74233d9d7cb3c574e3d30963c
Joshua Allen Bennett <jbn566@gmail.com>
2017-04-26 23:48:02 -0400

    Add net core version of Common Log

---
+++ CSharp/Framework/Net Core 1.1/JB2.Common/JB2.Common.Log/LogEntry.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JB2.Common;
+using JB2.Common.Enum;
+
+namespace JB2.Common.Log
+{
+    public  class LogEntry : LogEntry<string,LogServerityType>, ILogEntry
+    {
+
+        #region Constructors
+        public LogEntry(string id, LogServerityType serverity, string message, Exception exception, DateTime logDate,string logCode = "")
+            : base(id,serverity,message,exception,logDate,logCode)
+        {
+        }
+
+        #endregion Constructors
+
+        public static LogEntry NewLogEntry(LogServerityType serverity, string message,string logcode="")
+        {
+
+            return new LogEntry(JB2.Common.ShortGuid.NewGuid(), serverity, message,null,DateTime.Now,logcode);
+        }
+
+        public static LogEntry NewLogEntry(LogServerityType serverity, Exception exception,string logcode = "")
+        {
+            return new LogEntry(JB2.Common.ShortGuid.NewGuid(), serverity, exception.Message, exception, DateTime.Now,logcode);
+            
+        }
+ 
+    }
+
+
+
+    public class LogEntry<TKey,TServerity> : JB2Class, ILogEntry<TKey,TServerity>
+    {
+        #region Fields
+
+        private readonly Exception _exception;
+        private readonly TKey _id;
+        private readonly string _message;
+        private readonly DateTime _logDate;
+        private TServerity _serverity;
+        protected BaseCollection<Tag> _tags;
+
+        #endregion Fields
+
+        #region Constructor
+
+         public LogEntry(TKey id,TServerity serverity, string message,Exception exception,DateTime logDate,string logCode = "") :base ()
+        {
+            _exception = exception;
+            _id = id;
+            _logDate = logDate;
+            _message = message;
+            _serverity = serverity;
+            _props.SetProperty<string>("LogCode", logCode);
+            _tags = new BaseCollection<Tag>();
+
+        }
+
+        #endregion Constructor
+
+        #region Properties
+        public Exception Exception { get { return _exception; } }
+        public TKey ID { get { return _id; } }
+        public DateTime LogDate { get { return _logDate; } }
+        public string Message { get { return _message; } }
+        public TServerity Serverity { get { return _serverity; } }
+
+        public string LogCode
+        {
+            get
+            {
+                return _props.GetProperty<string>("LogCode", string.Empty);
+            }
+            set
+            {
+                _props.SetProperty<string>("LogCode", value);
+            }
+        }
+        #endregion Properties
+
+
+        #region ITags
+        public IEnumerable<Tag> GetTags()
+        {
+            if (_tags == null)
+                return new Tag[0];
+            return _tags;
+        }
+
+        public bool AddTag(Tag tag)
+        {
+            return _tags.Add(tag);
+        }
+
+        public bool RemoveTag(Tag tag)
+        {
+            return _tags.Remove(tag);
+        }
+
+        public JB2.Common.ServiceResult LoadTags(IEnumerable<Tag> tags)
+        {
+            try
+            {
+                var w = from t in tags select _tags.Add(t);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return new ServiceResult(ex);
+            }
+        }
+        #endregion ITags
+
+        public static LogEntry<TKey,TServerity> NewLogEntry(TKey id, TServerity serverity,string message)
+        {
+            return new LogEntry<TKey, TServerity>(id, serverity, message, null, DateTime.Now);
+        }
+        public static LogEntry<TKey, TServerity> NewLogEntry(TKey id, TServerity serverity, Exception exception)
+        {
+             return new LogEntry<TKey, TServerity>(id, serverity, exception.Message, exception, DateTime.Now);
+        }
+
+        
+    }
+}
---
+++ CSharp/Framework/Net Core 1.1/JB2.Common/JB2.Common.Log/Logger.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using JB2.Common;
+using JB2.Common.Enum;
+
+namespace JB2.Common.Log
+{
+    public abstract class Logger : ILogger
+    {
+        private Dictionary<Enum.LogServerityType, bool> _serverityFlag = new Dictionary<LogServerityType, bool>();
+
+
+        #region events
+        public event Action<ILogger<LogServerityType, string, ILogEntry>, LogServerityType, ILogEntry> EntryLogged;
+        public event Action<ILogger<LogServerityType, string, ILogEntry>, ILogEntry> InfomationalLogged;
+        public event Action<ILogger<LogServerityType, string, ILogEntry>, ILogEntry> WarningLogged;
+        public event Action<ILogger<LogServerityType, string, ILogEntry>, ILogEntry> ErrorLogged;
+        public event Action<ILogger<LogServerityType, string, ILogEntry>, ILogEntry> FatelLogged;
+        public event Action<ILogger<LogServerityType, string, ILogEntry>, ILogEntry> DebugLogged;
+        public event Action<ILogger<LogServerityType, string, ILogEntry>, ILogEntry> VerboseLogged;
+
+        public void OnEntryLogged(ILogger<LogServerityType, string, ILogEntry> logger, LogServerityType serverity, ILogEntry logEntry)
+        {
+            if (EntryLogged != null)
+                EntryLogged(logger, serverity, logEntry);
+        }
+
+        public void OnInformationalLogged(ILogger<LogServerityType, string, ILogEntry> logger, ILogEntry logEntry)
+        {
+            if (InfomationalLogged != null)
+                InfomationalLogged(logger, logEntry);
+        }
+
+        public void OnWarningLogged(ILogger<LogServerityType, string, ILogEntry> logger, ILogEntry logEntry)
+        {
+            if (WarningLogged != null)
+                WarningLogged(logger, logEntry);
+        }
+        public void OnErrorLogged(ILogger<LogServerityType, string, ILogEntry> logger, ILogEntry logEntry)
+        {
+            if (ErrorLogged != null)
+                ErrorLogged(logger, logEntry);
+        }
+        public void OnFatelLogged(ILogger<LogServerityType, string, ILogEntry> logger, ILogEntry logEntry)
+        {
+            if (FatelLogged != null)
+                FatelLogged(logger, logEntry);
+        }
+        public void OnDebugLogged(ILogger<LogServerityType, string, ILogEntry> logger, ILogEntry logEntry)
+        {
+            if (DebugLogged != null)
+                DebugLogged(logger, logEntry);
+        }
+        public void OnVerboseLogged(ILogger<LogServerityType, string, ILogEntry> logger, ILogEntry logEntry)
+        {
+            if (VerboseLogged != null)
+                VerboseLogged(logger, logEntry);
+        }
+
+
+        #endregion events
+
+        public virtual bool IsEnabled(LogServerityType serverity)
+        {
+            if (!_serverityFlag.ContainsKey(serverity))
+                return false;
+            else
+                return _serverityFlag[serverity];
+        }
+
+        public void SetAllServerity(bool enable)
+        {
+            _serverityFlag = new Dictionary<LogServerityType, bool>();
+            foreach(Enum.LogServerityType e in System.Enum.GetValues(typeof(Enum.LogServerityType)))
+            {
+                _serverityFlag.Add(e, enable);
+            }
+        }
+
+        public void SetServerityType(Enum.LogServerityType type, bool enable)
+        {
+            if (!_serverityFlag.ContainsKey(type))
+                _serverityFlag.Add(type, enable);
+            else
+                _serverityFlag[type] = enable;
+        }
+
+        public virtual void Log(ILogEntry entry)
+        {
+            if (this.EntryLogged != null)
+                EntryLogged(this, entry.Serverity, entry);
+
+            switch(entry.Serverity)
+            {
+                case LogServerityType.Debug:
+                    if (this.DebugLogged != null)
+                        DebugLogged(this, entry);
+                    break;
+                case LogServerityType.Error:
+                    if (this.ErrorLogged != null)
+                        ErrorLogged(this, entry);
+                    break;
+                case LogServerityType.Fatel:
+                    if (this.FatelLogged != null)
+                        FatelLogged(this, entry);
+                    break;
+                case LogServerityType.Informational:
+                    if (this.InfomationalLogged != null)
+                        InfomationalLogged(this, entry);
+                    break;
+                case LogServerityType.Verbose:
+                    if (this.VerboseLogged != null)
+                        VerboseLogged(this, entry);
+                    break;
+                case LogServerityType.Warning:
+                    if (this.WarningLogged != null)
+                        WarningLogged(this, entry);
+                    break;
+            }
+        }
+    }
+}

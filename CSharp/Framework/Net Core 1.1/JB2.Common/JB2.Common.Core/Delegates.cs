331b9ac62480c5124989fba0ed91e8fce04cbea6
Joshua Allen Bennett <jbn566@gmail.com>
2017-05-18 04:46:50 -0400

    Add Messager to Common Core .Net Core 1.1 lib

---
+++ CSharp/Framework/Net Core 1.1/JB2.Common/JB2.Common.Core/Delegates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Common
+{
+    public delegate Task<ServiceResult<TMessage>> SendMessage<TMessage, TMessageKey, TMessageTag>(IMessage<TMessageKey, TMessageTag> message)
+        where TMessage : IMessage<TMessageKey, TMessageTag>
+        where TMessageKey : IComparable;
+}
---
+++ CSharp/Framework/Net Core 1.1/JB2.Common/JB2.Common.Core/Interfaces/IEmailMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB2.Common
+{
+    public interface IEmailMessage : IMessage
+    {
+
+    }
+    public interface IEmailMessage<Tkey> : IMessage<Tkey>
+        where Tkey : IComparable
+    {
+
+    }
+    public interface IEmailMessage<Tkey, TTag> : IMessage<Tkey,TTag>
+        where Tkey : IComparable
+    {
+
+    }
+}
